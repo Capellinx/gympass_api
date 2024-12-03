255c55b07aae802cadefb6ae7c8b332330da02b8
capellinx <capellaaa7@gmail.com>
2024-12-03 20:43:27 -0300

    chore: setup configuration api

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import {z} from 'zod';
+
+
+export const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false) {
+  console.error('❌ Invalid environment variable', _env.error.format())
+  
+  throw new Error('Invalid environment variable')
+}
+
+export const env = _env.data
